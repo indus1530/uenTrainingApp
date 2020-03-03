7b3e1d32d70089b85880a6097711e052fd62efaf
SAJID LATIF <indus1530@gmail.com>
2020-03-03 19:23:50 +0500

    long time

--- app/src/main/java/com/example/abdulsajid/mnchtraining/FirstCard.java
+++ app/src/main/java/com/example/abdulsajid/mnchtraining/FirstCard.java
@@ -1,9 +1,9 @@
 package com.example.abdulsajid.mnchtraining;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class FirstCard extends AppCompatActivity implements View.OnClickListener {
+public class FirstCard extends Activity {
 
 
     Button btn1, btn2, btn3, btn4;
