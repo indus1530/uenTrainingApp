e3de1d82a52cf007f3430feb409d45ebb6923b67
SAJID LATIF <indus1530@gmail.com>
2020-03-15 14:36:15 +0500

    GdsPost

--- app/src/main/java/com/example/abdulsajid/mnchtraining/GDS/GdsPostTest.java
+++ app/src/main/java/com/example/abdulsajid/mnchtraining/GDS/GdsPostTest.java
@@ -465,9 +465,8 @@ public class GdsPostTest extends AppCompatActivity implements RadioButton.OnChec
 
 
         int temp = 0;
-        if (ed_cnic_no.getText().toString().length() > 0) {
-            cnic_no = ed_cnic_no.getText().toString().trim();
-        }
+
+        cnic_no = ed_cnic_no.getText().toString().length() > 0 ? ed_cnic_no.getText().toString().trim() : "0";
 
         //Q1
         if (rb_gdsposttestq1a1.isChecked()) {
