178dfa6d14708ae54b3e0308b849badd03cc96fa
SAJID LATIF <indus1530@gmail.com>
2020-07-05 23:27:35 +0500

    bindingTest on old sys

--- app/src/main/java/com/example/abdulsajid/mnchtraining/FirstCard.java
+++ app/src/main/java/com/example/abdulsajid/mnchtraining/FirstCard.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.abdulsajid.mnchtraining.R;
+
 public class FirstCard extends Activity {
 
 
